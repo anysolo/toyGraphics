92bb18eee653c69966ed61e446ad277159aabae9
anysolo-dlong <dlong@anysolo.com>
2020-11-14 15:36:28 -0500

    Used Pos and Size in events.

--- src/main/kotlin/com/anysolo/toyGraphics/events/window.kt
+++ src/main/kotlin/com/anysolo/toyGraphics/events/window.kt
@@ -1,17 +1,18 @@
 package com.anysolo.toyGraphics.events
 
 
+import com.anysolo.toyGraphics.Size
 import java.awt.event.ComponentAdapter
 import java.awt.event.ComponentEvent
 
 
 sealed class WindowEvent: Event
 
-data class WindowResizedEvent(val width: Int, val height: Int): WindowEvent()
+data class WindowResizedEvent(val size: Size): WindowEvent()
 
 
 internal class WindowEventAdapter(private val eventManager: EventManager): ComponentAdapter() {
     override fun componentResized(e: ComponentEvent) {
-        eventManager.putEvent(WindowResizedEvent(e.component.width, e.component.height))
+        eventManager.putEvent(WindowResizedEvent(Size(e.component.width, e.component.height)))
     }
 }
